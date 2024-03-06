1f130311d2f72069ec24738c664a79ef59abfe8c
Niksooh <dymitri.thomas@gmail.com>
2024-03-06 15:58:31 +0100

    Ajout variable + éléments objet + élement dom

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,3 +1,4 @@
+// Appel à la fonction fetch
 fetch ("http://localhost:5678/api/works").then ((data) => {
     return data.json()
 })
@@ -5,3 +6,21 @@ fetch ("http://localhost:5678/api/works").then ((data) => {
     console.log(works)
 })
 
+// variables
+let i = 0
+const portfolio = document.getElementById("portefolio")
+const figure = document.createElement("figure")
+const img = document.createElement("img")
+const figcaption = document.createElement("figcaption")
+
+ //// Création des éléments pour chaque objet
+ for(let i = 0; i < array.length; i++){
+    img.src = array[i].imageUrl
+    img.alt = array[i].title
+    figcaption.innerText = array[i].title
+
+    //// Ajout des éléments au DOM
+    gallery.appendChild(figure)
+    figure.appendChild(img)
+    figure.appendChild(figcaption)
+  }
