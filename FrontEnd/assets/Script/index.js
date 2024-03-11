e96b39ca203eef18bdfd6f377abbbd396f1e8f6c
Niksooh <dymitri.thomas@gmail.com>
2024-03-11 14:55:30 +0100

    Ajout création et affichage boutton filtres

--- FrontEnd/assets/Script/index.js
+++ FrontEnd/assets/Script/index.js
@@ -6,25 +6,40 @@ fetch ("http://localhost:5678/api/works").then ((data) => {
     console.log(works)
 })
 
+
 // variables
 let i = 0
 const portfolio = document.getElementById("portefolio")
 const figure = document.createElement("figure")
 const img = document.createElement("img")
 const figcaption = document.createElement("figcaption")
+const categoryFilters = document.createElement("article")
+  categoryFilters.classList.add("filters")
+const gallery = document.createElement("article")
+  gallery.classList.add("gallery")
+
+portfolio.appendChild(categoryFilters)
+portfolio.appendChild(gallery)
 
  //// Création des éléments pour chaque objet
- for(let i = 0; i < array.length; i++){
-    img.src = array[i].imageUrl
-    img.alt = array[i].title
-    figcaption.innerText = array[i].title
+ function displayWorks(array) {
+    gallery.innerHTML = ""
 
-    //// Ajout des éléments au DOM
-    gallery.appendChild(figure)
-    figure.appendChild(img)
-    figure.appendChild(figcaption)
+    //// Création des éléments pour chaque objet
+    for(let i = 0; i < array.length; i++){
+
+      img.src = array[i].imageUrl
+      img.alt = array[i].title
+      figcaption.innerText = array[i].title
+
+      //// Ajout des éléments au DOM
+      gallery.appendChild(figure)
+      figure.appendChild(img)
+      figure.appendChild(figcaption)
+    }
   }
-  
+
+
 //// Images/projets du portfolio à afficher
  function getWorks(){
     try{
@@ -37,3 +52,21 @@ const figcaption = document.createElement("figcaption")
     }
     return JSON.parse(sessionStorage.getItem("works"))
  }
+
+// Création et affichage des boutons de filtres par catégories
+ function displayCategoryFilters(array){
+    //// Par défaut, au premier chargement de la page, afficher l'intégralité de la galerie
+    displayWorks(array)
+    ////  Création du bouton d'affichage par défaut
+    const btn0 = document.createElement("button")
+      btn0.classList.add("btn","btn-category","unselectable","btn-selected")
+      btn0.id = 0
+      btn0.textContent = "Tous"
+    categoryFilters.appendChild(btn0)
+      //// Action au clic du bouton
+      btn0.addEventListener("click", ()=>{
+        removeFilterSelected()
+        displayWorks(array)
+        btn0.classList.add("btn-selected")
+      })
+    }
