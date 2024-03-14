c8af4de41491f74d12dc3d66fc50e676bd55173c
Niksooh <dymitri.thomas@gmail.com>
2024-03-14 12:45:53 +0100

    refonte du fichier + ajout du systeme de filtre

--- FrontEnd/assets/Script/index.js
+++ FrontEnd/assets/Script/index.js
@@ -1,53 +1,123 @@
-// Appel à la fonction fetch
-fetch("http://localhost:5678/api/works").then((data) => {
-    return data.json()
-})
-    .then((works) => {
-        console.log(works)
-        displayWorks(works)
-    })
+/* DÉCLARATION DES VARIABLES */
+let works = [];
+let categories = [];
+const galleryElement = document.querySelector(".gallery");
+const portfolio = document.getElementById("portfolio");
 
-fetch("http://localhost:5678/api/categories").then((data) => {
-    return data.json()
-})
-.then((categories) => {
-    console.log(categories)
-    categories.splice(0, 0, {name : "Tous"})
-    for (let i = 0; i < categories.length; i++) {
-        const button = document.createElement("button")
-        button.innerText = categories[i].name
-        document.getElementById("btn").appendChild(button)
+
+/* LISTE DES API */
+
+/**RÉCUPÉRATION API "works"(GALERIE) */
+fetch("http://localhost:5678/api/works")
+.then(response => {
+    if (!response.ok) {
+        throw Error(`${response.status}`)
     }
+    return response.json()
 })
+.then(galleryData => {
+    works = galleryData;
+    createWorks();
+})
+.catch(error => alert("Erreur : " + error))
 
-// variables
-let i = 0
-const portfolio = document.getElementById("portfolio")
-const categoryFilters = document.createElement("article")
-categoryFilters.classList.add("filters")
-const gallery = document.createElement("article")
-gallery.classList.add("gallery")
-
-portfolio.appendChild(categoryFilters)
-portfolio.appendChild(gallery)
-
-//// Création des éléments pour chaque objet
-function displayWorks(array) {
-    gallery.innerHTML = ""
-
-    //// Création des éléments pour chaque objet
-    for (let i = 0; i < array.length; i++) {
-        const figure = document.createElement("figure")
-        const img = document.createElement("img")
-        const figcaption = document.createElement("figcaption")
-        img.src = array[i].imageUrl
-        img.alt = array[i].title
-        figcaption.innerText = array[i].title
-
-        //// Ajout des éléments au DOM
-        gallery.appendChild(figure)
-        figure.appendChild(img)
-        figure.appendChild(figcaption)
+/**RÉCUPÉRATION API "categories" */
+fetch("http://localhost:5678/api/categories")
+.then(response => {
+    if (!response.ok) {
+        throw Error(`${response.status}`)
     }
-}
+    return response.json()
+})
+.then(categoriesData => {
+    categories = categoriesData;
+    createFilter();
+})
+.catch(error => alert("Erreur : " + error))
+
+
+/* DÉCLARATION DE FONCTIONS *
+
+        /**GALERIE */
+
+/**Fonction SUPPRIMER l'ancienne image HTML */
+function deleteWorks (){
+    galleryElement.innerHTML = "";
+};
+
+/**Fonction CRÉER une nouvelle API de travaux*/
+function createWorks(categoryId = null){
+    /**Delete gallery(works) */
+    deleteWorks()
+
+    /**Déterminez quel tableau fonctionne à utiliser en fonction de la catégorie */
+    const displayWorks = categoryId ? works.filter(work => work.categoryId === categoryId) : works;
+
+    /**Parcourez chaque œuvre en boucle */
+    displayWorks.forEach(work => {
+            /**Créer un élément <figure> pour chaque œuvre */
+            const figureGallery = document.createElement("figure");
+            figureGallery.setAttribute("id", `work-${work.id}`);
+            /**Créer un élément <img> pour afficher l'image de l'œuvre */
+            const imageElementGallery = document.createElement("img");
+            imageElementGallery.src = work.imageUrl;
+            imageElementGallery.setAttribute("alt", work.title);
+            /**Créer un élément <figcaption> pour afficher le titre de l'œuvre */
+            const titleImageGallery = document.createElement("figcaption");
+            titleImageGallery.innerText = work.title;
+            /**Ajouter les éléments à la galerie */
+            galleryElement.appendChild(figureGallery);
+            figureGallery.appendChild(imageElementGallery);
+            figureGallery.appendChild(titleImageGallery);
+    });
+};
+
+        /**FILTRE */
+
+/**Ajout de filtres de catégories pour filtrer les œuvres dans la galerie */
+function createFilter(){
+    /**Ajout d'une catégorie par défaut "Tous" (Tous)*/
+    categories.unshift({id: 0, name: "Tous"});
+
+    /**Création d'un élément <div> pour les catégories */
+    const categoriesElementFilter = document.createElement("div");
+    categoriesElementFilter.classList.add("categories");
+    portfolio.insertBefore(categoriesElementFilter, galleryElement);
+
+    /**Parcourez chaque catégorie */
+    categories.forEach((categoryElement, i) => {
+        /**Create buttons for different categories */
+        const categoryBtnFilter = document.createElement("button");
+        categoryBtnFilter.innerText = categoryElement.name;
+        categoryBtnFilter.value = categoryElement.id;
+        categoryBtnFilter.classList.add("category-btn");
+        /**Ajouter une classe au premier bouton */
+        if(i === 0){
+            categoryBtnFilter.classList.add("category-selected")
+        };
+        /**Ajouter des boutons aux catégories <div> */
+        categoriesElementFilter.appendChild(categoryBtnFilter);
+
+        /**Changer de catégorie avec la fonction addEventListener cliquez */
+        categoryBtnFilter.addEventListener("click", (e) => {
+            /**Obtenir l'ID de la catégorie sélectionnée */
+            const selectedCategoryId = parseInt(e.target.value);
+
+            /**Mettre à jour la galerie */
+            createWorks(selectedCategoryId);
+
+            /**changer la couleur du bouton */
+            const filterColorCategory = document.querySelectorAll(".category-btn");
+            filterColorCategory.forEach((filterColor, i) => {
+                if(i === selectedCategoryId){
+                    filterColor.classList.add("category-selected")
+                } else {
+                    if(filterColor.classList.contains("category-selected")){
+                        filterColor.classList.remove("category-selected")
+                    };
+                };
+            });
+        });
+    });
+};
 
