99d374af7898600994c8bf9a908d8281d8bcd9f5
Niksooh <dymitri.thomas@gmail.com>
2024-03-18 18:36:56 +0100

    Retrait des commentaire superflux

--- FrontEnd/assets/Script/index.js
+++ FrontEnd/assets/Script/index.js
@@ -1,13 +1,11 @@
 /* DÉCLARATION DES VARIABLES */
 let works = [];
-let categories = [];
 const galleryElement = document.querySelector(".gallery");
 const portfolio = document.getElementById("portfolio");
 
 
 /* LISTE DES API */
 
-/**RÉCUPÉRATION API "works"(GALERIE) */
 fetch("http://localhost:5678/api/works")
 .then(response => {
     if (!response.ok) {
@@ -21,7 +19,6 @@ fetch("http://localhost:5678/api/works")
 })
 .catch(error => alert("Erreur : " + error))
 
-/**RÉCUPÉRATION API "categories" */
 fetch("http://localhost:5678/api/categories")
 .then(response => {
     if (!response.ok) {
@@ -30,83 +27,66 @@ fetch("http://localhost:5678/api/categories")
     return response.json()
 })
 .then(categoriesData => {
-    categories = categoriesData;
-    createFilter();
+    createFilter(categoriesData);
 })
 .catch(error => alert("Erreur : " + error))
 
 
-/* DÉCLARATION DE FONCTIONS *
+/* DÉCLARATION DE FONCTIONS */
 
-        /**GALERIE */
-
-/**Fonction SUPPRIMER l'ancienne image HTML */
 function deleteWorks (){
     galleryElement.innerHTML = "";
 };
 
-/**Fonction CRÉER une nouvelle API de travaux*/
 function createWorks(categoryId = null){
-    /**Delete gallery(works) */
     deleteWorks()
 
-    /**Déterminez quel tableau fonctionne à utiliser en fonction de la catégorie */
-    const displayWorks = categoryId ? works.filter(work => work.categoryId === categoryId) : works;
+    if(categoryId) {
+        const displayWorks = works.filter(work => work.categoryId === categoryId)
+    }else {
+        const displayWorks = works
+    }
 
-    /**Parcourez chaque œuvre en boucle */
+
     displayWorks.forEach(work => {
-            /**Créer un élément <figure> pour chaque œuvre */
+
             const figureGallery = document.createElement("figure");
             figureGallery.setAttribute("id", `work-${work.id}`);
-            /**Créer un élément <img> pour afficher l'image de l'œuvre */
             const imageElementGallery = document.createElement("img");
             imageElementGallery.src = work.imageUrl;
             imageElementGallery.setAttribute("alt", work.title);
-            /**Créer un élément <figcaption> pour afficher le titre de l'œuvre */
             const titleImageGallery = document.createElement("figcaption");
             titleImageGallery.innerText = work.title;
-            /**Ajouter les éléments à la galerie */
             galleryElement.appendChild(figureGallery);
             figureGallery.appendChild(imageElementGallery);
             figureGallery.appendChild(titleImageGallery);
     });
 };
 
-        /**FILTRE */
+    /**FILTRE */
 
-/**Ajout de filtres de catégories pour filtrer les œuvres dans la galerie */
-function createFilter(){
-    /**Ajout d'une catégorie par défaut "Tous" (Tous)*/
+function createFilter(categories){
     categories.unshift({id: 0, name: "Tous"});
 
-    /**Création d'un élément <div> pour les catégories */
     const categoriesElementFilter = document.createElement("div");
     categoriesElementFilter.classList.add("categories");
     portfolio.insertBefore(categoriesElementFilter, galleryElement);
 
-    /**Parcourez chaque catégorie */
     categories.forEach((categoryElement, i) => {
-        /**Create buttons for different categories */
         const categoryBtnFilter = document.createElement("button");
         categoryBtnFilter.innerText = categoryElement.name;
         categoryBtnFilter.value = categoryElement.id;
         categoryBtnFilter.classList.add("category-btn");
-        /**Ajouter une classe au premier bouton */
         if(i === 0){
             categoryBtnFilter.classList.add("category-selected")
         };
-        /**Ajouter des boutons aux catégories <div> */
         categoriesElementFilter.appendChild(categoryBtnFilter);
 
-        /**Changer de catégorie avec la fonction addEventListener cliquez */
         categoryBtnFilter.addEventListener("click", (e) => {
-            /**Obtenir l'ID de la catégorie sélectionnée */
             const selectedCategoryId = parseInt(e.target.value);
 
-            /**Mettre à jour la galerie */
             createWorks(selectedCategoryId);
 
-            /**changer la couleur du bouton */
             const filterColorCategory = document.querySelectorAll(".category-btn");
             filterColorCategory.forEach((filterColor, i) => {
                 if(i === selectedCategoryId){
